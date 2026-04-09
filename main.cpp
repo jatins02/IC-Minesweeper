d436253b93e3666749b2360bf802c9b8fb7f5cba
amishanainwani123 <nainwaniamisha@gmail.com>
2026-04-09 10:42:53 +0530

    add functionality to difficulty setting buttons

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 const unsigned int WIDTH = 560;
 const unsigned int HEIGHT = 690;
 
+typedef enum { none, easy, mid, hard } difficulties;
+
 class DifficultyBtn : public sf::RectangleShape {
 private:
 	const int BTN_WIDTH = 440;
@@ -22,8 +24,8 @@ public:
 	}
 
 
-	DifficultyBtn(std::string text, sf::Font& font, float yPos)			// get an error saying, no default constructor for the sf::Text class, this is because, you have to pass in the font to the text class as soon as the constructor is called.
-		: content(font, text, 22) {					// you can pass in the text string as well as the size of characters while initialising sf::Text
+	DifficultyBtn(std::string text, sf::Font &font, float yPos)			// get an error saying, no default constructor for the sf::Text class, this is because, you have to pass in the font to the text class as soon as the constructor is called.
+		: content(font, text, 22){					// you can pass in the text string as well as the size of characters while initialising sf::Text
 
 		this->setSize(sf::Vector2f(BTN_WIDTH, BTN_HEIGHT));
 		this->setOrigin(this->getGeometricCenter());
@@ -40,7 +42,7 @@ public:
 };
 
 
-void setting_btn_clicked(int x, int y, DifficultyBtn& easy, DifficultyBtn& mid, DifficultyBtn& hard);
+void setting_btn_clicked(int x, int y, DifficultyBtn &easy, DifficultyBtn &mid, DifficultyBtn &hard, difficulties &difficulty_setting);
 
 
 int main() {
@@ -54,13 +56,20 @@ int main() {
 	}
 
 	sf::Text heading(font, "MINESWEEPER", 44);
-	heading.setPosition({ WIDTH / 2.0f - (44 * 11 / 2), 100 });
+	heading.setPosition({ WIDTH / 2.0f - (44*11/2), 100});
+
+
+	sf::Text newthings(font, "mb the best bae", 44);
+	newthings.setPosition({ WIDTH / 2.0f - (44 * 15 / 2), 121 });
+
+	difficulties difficulty_setting = none;
 
 	// INSTANTIATE THE BUTTONS
 
 	DifficultyBtn hardBtn("HARD", font, 600.0f);
 	DifficultyBtn midBtn("MID", font, 450.0f);
 	DifficultyBtn easyBtn("EASY", font, 300.0f);
+	
 
 	while (window.isOpen()) {
 		while (std::optional event = window.pollEvent()) {
@@ -69,7 +78,7 @@ int main() {
 			}
 
 			else if (auto* keypressed = event->getIf<sf::Event::KeyPressed>()) {
-				if (keypressed->scancode == sf::Keyboard::Scancode::Escape) {
+				if (keypressed->scancode == sf::Keyboard::Scancode::Escape){
 					window.close();
 				}
 			}
@@ -79,8 +88,10 @@ int main() {
 					int mouse_x = mouseBtnPressed->position.x;
 					int mouse_y = mouseBtnPressed->position.y;
 
-					setting_btn_clicked(mouse_x, mouse_y, easyBtn, midBtn, hardBtn);
-
+					if (difficulty_setting == none) {
+						setting_btn_clicked(mouse_x, mouse_y, easyBtn, midBtn, hardBtn, difficulty_setting);
+						std::cout << "current difficulty setting set to " << difficulty_setting << std::endl;
+					}
 				}
 			}
 		}
@@ -90,17 +101,24 @@ int main() {
 		window.clear(sf::Color::Magenta);
 
 		// draw objects
-		window.draw(easyBtn);
-		window.draw(midBtn);
-		window.draw(hardBtn);
 
-		window.draw(easyBtn.content);
-		window.draw(midBtn.content);
-		window.draw(hardBtn.content);
+		if (difficulty_setting == none) {
+			window.draw(easyBtn);
+			window.draw(midBtn);
+			window.draw(hardBtn);
 
-		window.draw(heading);
+			window.draw(easyBtn.content);
+			window.draw(midBtn.content);
+			window.draw(hardBtn.content);
 
+			window.draw(heading);
+		}
 
+		else {
+			// draw the game grid window
+			window.draw(newthings);
+		}
+		
 		window.display();
 
 	}
@@ -109,17 +127,25 @@ int main() {
 }
 
 
-void setting_btn_clicked(int x, int y, DifficultyBtn& easy, DifficultyBtn& mid, DifficultyBtn& hard) {
+void setting_btn_clicked(int x, int y, DifficultyBtn &easy, DifficultyBtn &mid, DifficultyBtn &hard, difficulties &difficulty_setting) {
 
 	sf::Vector2f mousePos((float)x, (float)y);
 
 	if (easy.getGlobalBounds().contains(mousePos)) {
-		std::cout << "easy was clicked" << std::endl;
+		//std::cout << "easy was clicked" << std::endl;
+		difficulty_setting = difficulties::easy;
+		// start game in easy mode
 	}
 	else if (mid.getGlobalBounds().contains(mousePos)) {
-		std::cout << "mid was clicked" << std::endl;
+		//std::cout << "mid was clicked" << std::endl;
+		difficulty_setting = difficulties::mid;
+		// start game in medium mode
 	}
 	else if (hard.getGlobalBounds().contains(mousePos)) {
-		std::cout << "hard was clicked" << std::endl;
+		//std::cout << "hard was clicked" << std::endl;
+		difficulty_setting = difficulties::hard;
+		// start game in hard mode
 	}
+
+
 }
